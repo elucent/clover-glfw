107c804adbb75e1d5dfe58ea884c9a3e70207e69
elucent <9532786+elucent@users.noreply.github.com>
2026-06-27 12:01:03 -0700

    Add shims with nicer names and typed mangling for GLFW functions.

--- genfuncs.py
+++ genfuncs.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 replacements = {
     '(void)': '()',
@@ -66,25 +67,53 @@ def adaptName(name):
     name = name.lstrip("(").rstrip(")")
     if name.startswith("GLFW_"):
         return name[5:] # strip off the GLFW_ part
+    if name.startswith("glfw"):
+        name = name[4:]
+        name = name[0].lower() + name[1:]
+        return name
     return name
 
 with open(sys.argv[2], "w") as out:
     out.write("use glfwtypes\n")
     out.write("in glfw:\n")
+    natives = ''
+    nicenames = ''
     with open(sys.argv[1], "r") as f:
         for line in f.readlines():
             line = line.lstrip()
             if line.startswith("GLFWAPI "):
-                terms = line.split()
-                name = 2
-                while terms[name - 1] in modifiers:
-                    name += 1
-                nameparts = terms[name].split('(')
-                nameparts[0] = f"`{nameparts[0]}`"
-                terms[name] = nameparts[0] + '(' + nameparts[1]
-                result = ' '.join(terms[1:])
+                lparen = line.index('(')
+                rparen = line.index(')')
+                params = line[lparen:rparen + 1]
+                parameterNames = []
+                paramComponents = filter(lambda x: len(x) > 0, re.split(r'[\(\)\,]', params))
+                for var in paramComponents:
+                    paramNameIndex = len(var) - 1
+                    while paramNameIndex >= 0 and not var[paramNameIndex].isspace() and var[paramNameIndex] != '*':
+                        paramNameIndex -= 1
+                    if paramNameIndex >= 0:
+                        parameterNames.append(var[paramNameIndex + 1:])
+
+                namestart = lparen - 1
+                while not line[namestart].isspace() and line[namestart] != '*':
+                    namestart -= 1
+                name = line[namestart + 1:lparen]
+
+                returnType = line[8:namestart]
+
+                quotedName = f"`{name}`"
+                niceName = adaptName(name)
+
+                nativeDef = f'        {returnType} {quotedName}{params}\n'
+                niceDef = f'    {returnType} {niceName}{params}: native.{name}({', '.join(parameterNames)})\n'
+                if 'VK' in nativeDef or 'vk' in nativeDef or 'Vk' in nativeDef:
+                    continue
                 for k, v in replacements.items():
-                    result = result.replace(k, v, -1)
-                if 'vk' in result or 'Vk' in result:
-                    result = "# " + result
-                out.write('    ' + result + '\n')
+                    nativeDef = nativeDef.replace(k, v, -1)
+                    niceDef = niceDef.replace(k, v, -1)
+
+                natives += nativeDef
+                nicenames += niceDef
+    out.write('    in native:\n')
+    out.write(natives)
+    out.write(nicenames)
